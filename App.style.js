ed5c70eba4172ed1809320440936c646cf9b8c63
Pratik Devnal <pratik.devnal@gmail.com>
2023-01-25 21:13:01 +0530

    created repo Duolingo-clone

---
+++ App.style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    root:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    text:{
+        fontSize:30,
+        fontWeight:"bold"
+    },
+})
+
+export default styles;
